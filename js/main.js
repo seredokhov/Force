c95085b45f0e57ae2ccbdeb49413fb4c210100d5
seredokhov <seredokhovp@mail.ru>
2017-05-22 00:00:45 +0200

    test

--- js/main.js
+++ js/main.js
@@ -116,3 +116,11 @@ table.click(function(){
 	}
 })
 
+
+
+$('.dropdown.keep-open').on({
+    "shown.bs.dropdown": function() { this.closable = false; },
+    "click":             function() { this.closable = true; },
+    "hide.bs.dropdown":  function() { return this.closable; }
+});
+
